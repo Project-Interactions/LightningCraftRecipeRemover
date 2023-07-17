780e551006850f4a09a44228aade3e6fadb49302
mczph <mczphmodpack@163.com>
2023-07-17 17:25:01 +0800

    final

--- src/main/java/git/mczph/ltrr/Main.java
+++ src/main/java/git/mczph/ltrr/Main.java
@@ -1,9 +1,9 @@
-package git.mczph.lcrr;
+package git.mczph.ltrr;
 
-import sblectric.lightningcraft.recipes.LightningTransformRecipes;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import sblectric.lightningcraft.recipes.LightningTransformRecipes;
 
 @Mod(
         modid = Main.MODID,
@@ -13,17 +13,17 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 )
 
 public class Main {
-    public static final String MODID = "lcrr";
-    public static final String NAME = "LightningCraft Recipe Remover";
+    public static final String MODID = "ltrr";
+    public static final String NAME = "LightningTransform Recipe Remover";
     public static final String VERSION = "1.0.0";
     public static final String dependencies = "required-after:lightningcraft";
 
     @EventHandler
     public void onPreInit(FMLPreInitializationEvent event) {
-        modifyLightningCraftDefaultRecipes();
+        removeLightningCraftDefaultRecipes();
     }
 
-    private void modifyLightningCraftDefaultRecipes() {
+    private void removeLightningCraftDefaultRecipes() {
         LightningTransformRecipes.instance().getRecipeList().clear();
     }
 }
